e19a2764b072d0d56d8ba879e263bfe811effea4
Massimo Bourquin <massimo.bourquin@epfl.ch>
2023-07-07 14:24:19 +0200

    modif combinedata

--- 1_CreateData.py
+++ 1_CreateData.py
@@ -3,6 +3,14 @@ from code.b_CreateGlaciers import MainB
 from code.c_CombineData import MainC
 
 def Main():
+    print('Running a_CreateClimate.py ...')
     MainA() # Create climate data
+
+    print('Running b_CreateGlaciers.py ...')
     MainB() # Create glacier data
+
+    print('Running c_CombineData.py ...')
     MainC() # Combine climate, glacier and stream data
+
+if __name__ == "__main__":
+    Main()
